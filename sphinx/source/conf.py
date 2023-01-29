87975a74cab38205fd8404cfdc310e63d238e105
vital14 <limavictor49@gmail.com>
2023-01-28 22:32:12 -0300

    etapa 4

---
+++ sphinx/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Trabalho-Individual-2022-2'
+copyright = '2023, Vital14'
+author = 'Vital14'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['breathe']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pt'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+breathe_projects = {"Trabalho-Individual-2022-2": "../doxygen-docs"}
+breathe_default_project = "Trabalho-Individual-2022-2"
